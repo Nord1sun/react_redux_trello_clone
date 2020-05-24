ae8f2d48b8555d0bed132f455d9cd89377ca71cd
Nord1sun <65829009+Nord1sun@users.noreply.github.com>
2020-05-24 14:46:11 +0300

    Update Login.js

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -16,7 +16,7 @@ class Login extends Component {
       <div className="Login container">
         <div className="row justify-content-center">
           <div className="col-md-6">
-            <h2 className="text-center">Welcome to Djello Project Management!</h2>
+            <h2 className="text-center">Welcome to PlanX!</h2>
             <h6 className="text-center">Please login to continue</h6>
             {sessionError.message && <Alert color="danger">{sessionError.message}</Alert>}
             <Card>
